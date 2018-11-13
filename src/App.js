8d5dba571c3cbd6433c8224fa79ce574bbb8eef5
Leszek <lech.cirmirakis@webcrafters.pl>
2018-11-13 18:55:13 +0100

    change person data from props

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
           <h1>Hi, I am React App :)</h1>
           <p>This is really working</p>
-          <Person/>
-          <Person/>
-          <Person/>
+          <Person name="Lech" age="34"/>
+          <Person name="Malusinska" age="30">My hobby Is Raicing</Person>
+          <Person name="Andrzej" age="45"/>
       </div>
     )
     // return  React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I am React Header 1'))
