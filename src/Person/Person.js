941856619a420ec3a08545fa78eecb6ab4773639
Leszek <lech.cirmirakis@webcrafters.pl>
2018-11-13 19:01:27 +0100

    add props.children to person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>I`m a {props.name} and i have a {props.age} years old</p>
+    return (
+        <div>
+            <p>I`m a {props.name} and i have a {props.age} years old</p>
+            {/* props children reprezentuje wszystko co się zawiera miedyz znacznikami danego componentu
+            może to być zwykły teks, może to być kod JavaSvript albo inny component react itp... */}
+            <p>{props.children}</p>
+        </div>
+       
+        
+        )
 }
 
 export default person;
