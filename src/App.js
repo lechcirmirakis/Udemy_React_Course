ec3daa1778970b300bb3211a9b0190df4fa085b5
Leszek <lech.cirmirakis@webcrafters.pl>
2018-11-20 15:09:49 +0100

    outputting list dynamicly from state bym map method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
+import person from './Person/Person.js';
 
 // Component zbudowany za pomoca dziedziczenia z Reactowego Objectu Component
 class App extends Component {
@@ -54,21 +55,18 @@ class App extends Component {
     };
 
     let persons = null;
-
+    
     if (this.state.showPersons) {
       persons = (
         <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Maksymilian")}
-              changed={this.nameChangedHandler}>My hobby Is Raicing</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
+          {
+            this.state.persons.map(person => {
+              return <Person
+                name={person.name}
+                age={person.age}
+                />
+            })
+          }
           </div> 
       )
     }
